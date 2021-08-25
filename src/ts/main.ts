1f8fdd6d17a318b76e15782725a174e95352867d
coffmanjrp <coffmanjrp@gmail.com>
2021-08-25 09:32:27 -0700

    Fix event target type

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,8 @@
 const bg = document.getElementById('background') as HTMLDivElement;
 const password = document.getElementById('password') as HTMLInputElement;
 
-function clearBlur(e) {
-  const input = e.target.value;
+function clearBlur(e: Event) {
+  const input = (e.target as HTMLInputElement).value;
   const { length } = input;
   const blurValue = 20 - length * 2;
 
